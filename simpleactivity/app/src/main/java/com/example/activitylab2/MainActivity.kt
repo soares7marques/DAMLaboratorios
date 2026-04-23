7d03d6f98a7d679521b068e258aa8c1e015dca99
Soares <soaresmarquesjose1@email.com>
2026-04-23 07:42:34 +0100

    activity simples feito

---
+++ simpleactivity/app/src/main/java/com/example/activitylab2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.activitylab2
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    /**
+     * Método vinculado ao atributo android:onClick="finishButtonPressed" no XML.
+     * Quando o botão for clicado, este método será executado.
+     */
+    fun finishButtonPressed(view: View) {
+        // Encerra a atividade atual
+        finish()
+    }
+}
